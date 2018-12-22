3748a2dce4fbd0758721439b3d512cb2647caf60
Duc Pham <bloomsybusiness@yahoo.com>
2018-12-22 12:10:12 -0700

    finished header and nav responsive

--- src/Components/App.js
+++ src/Components/App.js
@@ -10,6 +10,7 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons'
 import axios from "axios"
+import "../ComponentStyles/App.css"
 var queryString = require('querystring')
 library.add(fab, faCheckSquare, faCoffee);
 
